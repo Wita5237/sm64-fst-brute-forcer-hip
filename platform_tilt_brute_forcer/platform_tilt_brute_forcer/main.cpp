b7467a59cfd8c9e9d5aaa0e23fdd87ad574ead07
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2023-02-16 12:17:21 +0000

    Improved camera angle range check
    Previous method could fail if the angle range was very large. Might cause problems for low speed solutions.

--- platform_tilt_brute_forcer/platform_tilt_brute_forcer/main.cpp
+++ platform_tilt_brute_forcer/platform_tilt_brute_forcer/main.cpp
@@ -1513,23 +1513,24 @@ __device__ bool test_one_up_position(int solIdx, float* startPosition, float* on
     float cameraPositions[4][3] = { {-8192, -2918, -8192}, {-8192, -2918, 8191}, {8191, -2918, -8192}, {8191, -2918, 8191} };
     bool foundSolution = false;
 
-    int minCameraYaw = INT_MAX;
-    int maxCameraYaw = -INT_MAX;
+    int minCameraYaw = 0;
+    int maxCameraYaw = 0;
 
-    for (int k = 0; k < 4; k++) {
+    int refCameraYaw = calculate_camera_yaw(oneUpPlatformPosition, cameraPositions[0]);
+    refCameraYaw = (65536 + refCameraYaw) % 65536;
+
+    for (int k = 1; k < 4; k++) {
         int cameraYaw = calculate_camera_yaw(oneUpPlatformPosition, cameraPositions[k]);
-        cameraYaw = (65536 + cameraYaw) % 65536;
+        cameraYaw = cameraYaw - refCameraYaw;
         minCameraYaw = min(minCameraYaw, cameraYaw);
         maxCameraYaw = max(maxCameraYaw, cameraYaw);
     }
 
-    int minCameraIdx = gReverseArctanTable[minCameraYaw];
-    int maxCameraIdx = gReverseArctanTable[maxCameraYaw];
+    int minCameraIdx = gReverseArctanTable[(65536 + minCameraYaw + refCameraYaw) % 65536];
+    int maxCameraIdx = gReverseArctanTable[(65536 + maxCameraYaw + refCameraYaw) % 65536];
 
-    if (maxCameraYaw - minCameraYaw > 32768) {
-        int temp = maxCameraIdx;
-        maxCameraIdx = 8192 + minCameraIdx;
-        minCameraIdx = temp;
+    if (minCameraIdx > maxCameraIdx) {
+        maxCameraIdx += 8192;
     }
 
     for (int cIdx = minCameraIdx; cIdx <= maxCameraIdx; cIdx++) {
